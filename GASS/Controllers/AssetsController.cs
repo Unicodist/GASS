12a0e659aa32870deb6e5ecdced9b2127a4e434f
Unicodist <47056442+Unicodist@users.noreply.github.com>
2021-04-08 13:28:57 +0545

    V4.3
    Asset file now gets uploaded to the server :)

--- GASS/Controllers/AssetsController.cs
+++ GASS/Controllers/AssetsController.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GASS.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GASS.Controllers
 {
     public class AssetsController : Controller
     {
         GASSDBContext dal = new GASSDBContext();
-        
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public AssetsController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
         public IActionResult Category(string id="new")
         {
 
@@ -26,14 +34,21 @@ namespace GASS.Controllers
         }
         public IActionResult UploadAssetAction(AssetUploadViewModel fileInfo)
         {
+            string unFileName = null;
+            string uploadFolder = Path.Combine(hostingEnvironment.WebRootPath, "UserData","assetFiles");
+            unFileName = "GB_"+Guid.NewGuid().ToString()+"_"+fileInfo.assetFile.FileName;
+            string filePath = Path.Combine(uploadFolder, unFileName);
+            fileInfo.assetFile.CopyTo(new FileStream(filePath, FileMode.Create));
+            
             IFormFile file = fileInfo.assetFile;
             return Content(Convert.ToInt32(file.Length)/1024 + "KB");
         }
-        public String UploadCat(string title, string description)
+        public async Task<String> UploadCat(string title, string description)
         {
-            if (title.Equals(String.Empty) || description.Equals(String.Empty)) return "error";
+            if (title.Equals(String.Empty)) return "error";
             try
             {
+                if (dal.category.Where(x => x.category_name.Equals(title)).Count() > 0) return "error";
                 CategoryModel newcat = new CategoryModel
                 {
                     category_name = title,
@@ -41,11 +56,17 @@ namespace GASS.Controllers
                 };
                 dal.category.Add(newcat);
                 dal.SaveChanges();
-                return "good";
+                CategoryModel retCat = dal.category.SingleOrDefault(x => x.category_name.Equals(title));
+                return JsonConvert.SerializeObject(retCat);
             } catch (Exception)
             {
                 return "error";
             }
+        }
+        public async Task<string> getCategories(string name)
+        {
+            List<CategoryModel> categories = dal.category.Where(x => x.category_name.Contains(name)).ToList();
+            return JsonConvert.SerializeObject(categories);
         }
     }
 }
--- GASS/Models/CategoryModel.cs
+++ GASS/Models/CategoryModel.cs
@@ -14,7 +14,5 @@ namespace GASS.Models
         public int id { get; set; }
         public string category_name { get; set; }
         public string category_description { get; set; }
-
-        
     }
 }
